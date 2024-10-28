679bafee97cbaf1018f9b4d48462b0abc30ee85d
fadil-27 <fadilahmuhammad237@gmail.com>
2024-10-28 21:13:13 +0800

    PenghitungUmur

---
+++ src/main/java/com/mycompany/aplikasipenghitungumur/AplikasiPenghitungUmur.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasipenghitungumur;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AplikasiPenghitungUmur {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
